2fcb227b0cae5ac8d6a51b7058bd60828e7838c7
kluan01 <l.kaden.m@gmail.com>
2024-12-23 17:44:49 -0500

    Seperated generate_map & test_algorithms to different end points

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# from graphStuff import map_generator, graph_times <-- console version
-
-# def main():
-#     map_generator.generate_map()
-#     graph_times.test_algorithms()
-
-# if __name__ == "__main__":
-#     main()
-
 from flask import Flask, jsonify, send_file
 from calcGraph import map_generator, graph_times
 from flask_cors import CORS
@@ -15,10 +6,19 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/run-functions", methods=["POST"])
-def run_functions():
+# Endpoint to generate the map
+@app.route("/api/generate-map", methods=["POST"])
+def generate_map():
     try:
         map_generator.generate_map()
+        return jsonify({"message": "Map generated successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Endpoint to run algorithms
+@app.route("/api/test-algorithms", methods=["POST"])
+def test_algorithms():
+    try:
         results = graph_times.test_algorithms()
         return jsonify(results), 200
     except Exception as e:
